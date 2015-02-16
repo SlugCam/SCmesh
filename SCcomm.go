f59edb36b7901b1a6ee56f5f452cc528c1255d4a
Leland Miller <lelandmiller@gmail.com>
2015-02-16 20:43:53 +0000

    Uses a buffered channel to store messages from TCP. Updates to match changes in gowifly.

--- SCcomm.go
+++ SCcomm.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"github.com/lelandmiller/gowifly"
 	"log"
 	"net"
-	"sync"
+	"strings"
 )
 
-func listenClients() {
+// TODO should only accept from localhost
+func listenClients(mchan chan<- string) {
 	ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatal(err)
@@ -20,28 +20,47 @@ func listenClients() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		go handleConnection(conn)
+		go handleConnection(conn, mchan)
 	}
 }
 
-func handleConnection(c net.Conn) {
+// TODO terminate gracefully
+// TODO determine if trimming behavior is correct
+func handleConnection(c net.Conn, mchan chan<- string) {
 	reader := bufio.NewReader(c)
 	for {
 		reply, err := reader.ReadBytes('\n')
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println(string(reply))
+		mchan <- strings.Trim(string(reply), "\n\r ")
+		//fmt.Println(string(reply))
 	}
 	c.Close()
 }
 
 func main() {
-	go listenClients()
-	w := gowifly.Setup()
-	w.serialConn.write("GOGOGO\r")
+	// TODO is buffer size good? What happens if buffer full. Looked it up, it
+	// should buffer
+	mchan := make(chan string, 500)
+	go listenClients(mchan)
+	w := gowifly.NewWiFlyConnection()
+	//w.serialConn.write("GOGOGO\r")
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-	wg.Wait()
+	//var m string
+	for {
+		m := <-mchan
+		switch m {
+		case "comm":
+			w.EnterCommandMode()
+		default:
+			w.WriteCommand(m)
+		}
+	}
+
+	/*
+		var wg sync.WaitGroup
+		wg.Add(1)
+		wg.Wait()
+	*/
 }
