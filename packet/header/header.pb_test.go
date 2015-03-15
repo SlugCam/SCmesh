5156f160b4a5d8c8996cf4ef1b675576af436304
Leland Miller <lelandmiller@gmail.com>
2015-03-15 06:37:52 +0000

    Added benchmark for MD5.

--- packet/header/header.pb_test.go
+++ packet/header/header.pb_test.go
@@ -1,6 +1,7 @@
 package header
 
 import (
+	"crypto/md5"
 	"testing"
 
 	"github.com/golang/protobuf/proto"
@@ -13,5 +14,13 @@ func BenchmarkHello(b *testing.B) {
 
 		newHeader := &Header{}
 		_ = proto.Unmarshal(serializedHeader, newHeader)
+	}
+}
+
+func BenchmarkMD5(b *testing.B) {
+	test := make([]byte, 2000)
+	for i := 0; i < b.N; i++ {
+		a := md5.Sum(test)
+		_ = a[0]
 	}
 }
