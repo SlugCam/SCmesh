c112d23ed5a0534c4b1d7d219def2aacfc35a85c
Leland Miller <lelandmiller@gmail.com>
2015-03-26 02:29:11 -0700

    Added scclient for testing.

---
+++ scclient/scclient.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+)
+
+const POWER_SOCKET = "/tmp/paunix.str"
+
+type PowerReq struct {
+	Type string `json:"type"`
+	Data string `json:"data"`
+}
+
+func main() {
+	getBattery()
+	/*
+		conn, err := net.Dial("unixpacket", "/tmp/sc")
+		//check error TODO
+		fmt.Println("Hello")
+		fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+		status, err := bufio.NewReader(conn).ReadString('\n')
+	*/
+}
+
+/*
+func serve() {
+
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		// handle error
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// handle error
+		}
+		//go handleConnection(conn)
+	}
+}
+*/
+
+func getBattery() {
+	r := PowerReq{
+		Type: "status-request",
+		Data: "battery",
+	}
+	b, err := json.Marshal(r)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error marshalling request to json: %s\n", err)
+		return
+	}
+
+	conn, err := net.Dial("unix", POWER_SOCKET)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error opening power socket: %s\n", err)
+		return
+	}
+
+	fmt.Fprintf(conn, "%s\n", b)
+	status, err := bufio.NewReader(conn).ReadString('\n')
+	fmt.Println("RETURN:", status)
+
+}
