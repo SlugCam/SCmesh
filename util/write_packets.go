7f3894d5bbe83e52bba54e47147cfd60cc459813
Leland Miller <lelandmiller@gmail.com>
2015-05-03 12:57:26 -0700

    Reset wifly at intervals.

--- util/write_packets.go
+++ util/write_packets.go
@@ -1,20 +1,24 @@
 package util
 
 import (
+	log "github.com/Sirupsen/logrus"
 	"io"
 	"time"
 )
 
 func resetWiFly(out io.Writer) {
 	out.Write([]byte("$$$"))
-	time.Sleep(260 * time.Millisecond)
-	out.Write([]byte("reboot\r\n"))
+	time.Sleep(300 * time.Millisecond)
+	out.Write([]byte("\rreboot\r"))
 	time.Sleep(3 * time.Second)
 }
 
 // WritePackets writes byte slices to an io.Writer. Used as the last stage in
 // the pipeline.
 func WritePackets(in <-chan []byte, out io.Writer, resetTime time.Duration) {
+	log.Info("reseting WiFly")
+	resetWiFly(out)
+
 	t := time.NewTicker(resetTime)
 	out.Write([]byte{'\x04'}) // Send any extraneous data
 
@@ -28,8 +32,10 @@ func WritePackets(in <-chan []byte, out io.Writer, resetTime time.Duration) {
 			for {
 				select {
 				case c := <-in:
+					log.Info("sending packet")
 					out.Write(c)
 				case <-t.C:
+					log.Info("reseting WiFly")
 					resetWiFly(out)
 				}
 			}
